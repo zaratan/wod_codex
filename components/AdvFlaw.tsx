80d9c4df5fea5240ee5c9e13dea1094492e52238
Denis Zaratan Pasin <zaratan@hey.com>
2021-05-19 21:45:55 +0900

    End page and component split

--- components/AdvFlaw.tsx
+++ components/AdvFlaw.tsx
@@ -12,22 +12,29 @@ const AdvFlaw = ({ advFlaw }: { advFlaw: AdvFlawType }) => {
   return (
     <article className="py-6" id={slug}>
       <header className="flex flex-row items-baseline py-3">
-        <h5 className="pr-4 text-2xl flex flex-row items-baseline space-x-2 font-medium font-serif">
-          <span>{advFlaw.name}</span>
-          <span className="text-base">
-            {advFlaw.kind} :{' '}
-            {typeof advFlaw.level === 'number'
-              ? advFlaw.level
-              : advFlaw.level.join(' ou ')}
+        <h5 className="pr-4 text-2xl flex flex-col sm:flex-row items-baseline space-y-2 sm:space-x-2 font-medium font-serif">
+          <span className="flex flex-row space-x-2 items-baseline">
+            {advFlaw.name}
           </span>
-          <a href={`#${slug}`}>
+          <span className="text-base flex flex-row space-x-2">
+            <span>
+              {advFlaw.kind} :{' '}
+              {typeof advFlaw.level === 'number'
+                ? advFlaw.level
+                : advFlaw.level.join(' ou ')}
+            </span>
+            <a href={`#${slug}`} className="block sm:hidden">
+              <LinkIcon className="w-5 h-5 text-blue-700 dark:text-blue-400 opacity-75 hover:opacity-100" />
+            </a>
+          </span>
+          <a href={`#${slug}`} className="hidden sm:block">
             <LinkIcon className="w-5 h-5 text-blue-700 dark:text-blue-400 opacity-75 hover:opacity-100" />
           </a>
         </h5>
       </header>
       <p className="py-1">{advFlaw.description}</p>
       <footer className="text-xs flex justify-end w-full">
-        <span className="max-w-md pt-4">{advFlaw.source}</span>
+        <span className="sm:max-w-md pt-4">{advFlaw.source}</span>
       </footer>
     </article>
   );
--- pages/adv_flaws.tsx
+++ pages/adv_flaws.tsx
@@ -1,7 +1,8 @@
 import { LinkIcon } from '@heroicons/react/outline';
 import { GetStaticProps } from 'next';
 import React from 'react';
-import AdvFlaw from '../components/AdvFlaw';
+import AdvFlawCategory from '../components/AdvFlawCategory';
+import AdvFlawCategoryLinks from '../components/AdvFlawCategoryLinks';
 import Layout from '../components/Layout';
 import { loadAdvFlaws } from '../helpers/dataLoader';
 import { AdvFlawType } from '../types/AdvFlawTypes';
@@ -33,40 +34,12 @@ const AdvFlawPage = ({
     title="Avantages et inconvénients"
     description="Une liste de tous les avantages et inconvénients pour WoD"
   >
+    <AdvFlawCategoryLinks types={advFlawsGroups.map((e) => e.type)} />
     {advFlawsGroups.map((advFlawsGroup) => (
-      <section id={`type-${advFlawsGroup.type}`}>
-        <header className="text-3xl py-5">
-          <h3 className="flex flex-row space-x-2 items-center font-medium font-serif justify-center">
-            <span>{advFlawsGroup.type}</span>
-            <a href={`#type-${advFlawsGroup.type}`}>
-              <LinkIcon className="w-5 h-5 text-blue-700 dark:text-blue-400 opacity-75 hover:opacity-100" />
-            </a>
-          </h3>
-        </header>
-        <ul>
-          {advFlawsGroup.subtypes.map((advFlawsSubGroup) => (
-            <section
-              id={`type-${advFlawsSubGroup.type}-${advFlawsSubGroup.subtype}`}
-            >
-              <header className="text-3xl py-5">
-                <h4 className="flex flex-row space-x-2 items-center font-medium font-serif">
-                  <span>{advFlawsSubGroup.subtype}</span>
-                  <a
-                    href={`#type-${advFlawsSubGroup.type}-${advFlawsSubGroup.subtype}`}
-                  >
-                    <LinkIcon className="w-5 h-5 text-blue-700 dark:text-blue-400 opacity-75 hover:opacity-100" />
-                  </a>
-                </h4>
-              </header>
-              <ul>
-                {advFlawsSubGroup.data.map((advFlaws) => (
-                  <AdvFlaw advFlaw={advFlaws} />
-                ))}
-              </ul>
-            </section>
-          ))}
-        </ul>
-      </section>
+      <AdvFlawCategory
+        advFlawsGroup={advFlawsGroup}
+        key={`type-${advFlawsGroup.type}`}
+      />
     ))}
   </Layout>
 );
