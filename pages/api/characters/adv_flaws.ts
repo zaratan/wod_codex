229fe31d745afc86d01ef90b676cf20a77b25573
Denis Zaratan Pasin <zaratan@hey.com>
2021-05-21 04:43:30 +0900

    characters api: flaws

--- pages/api/characters/adv_flaws.ts
+++ pages/api/characters/adv_flaws.ts
@@ -1,8 +1,37 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { loadAdvFlaws } from '../../../helpers/dataLoader';
 import runCors from '../../../helpers/runCors';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   await runCors(req, res);
 
-  res.json({ message: 'Hello Everyone!' });
+  const advFlaws = loadAdvFlaws();
+
+  const results = advFlaws
+    .map((e) => e.subtypes.map((f) => f.data))
+    .flat(2)
+    .reduce<{
+      advantages: Array<{ name: string; path: string; levels: Array<number> }>;
+      flaws: Array<{ name: string; path: string; levels: Array<number> }>;
+    }>(
+      (result, nextAdvFlaw) => {
+        const newRes = {
+          name: nextAdvFlaw.name,
+          path: ``,
+          levels:
+            typeof nextAdvFlaw.level === 'number'
+              ? [nextAdvFlaw.level]
+              : nextAdvFlaw.level,
+        };
+        if (nextAdvFlaw.kind === 'Atout') {
+          result.advantages.push(newRes);
+        } else {
+          result.flaws.push(newRes);
+        }
+        return result;
+      },
+      { advantages: [], flaws: [] }
+    );
+
+  res.json(results);
 };
